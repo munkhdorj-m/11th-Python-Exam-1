6d655294f2d1049060dcb36b50474b3a0a818873
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2025-11-24 23:33:00 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,21 +1,17 @@
 # Exercise 1
 def second_last_digit_is_5(n):
-    # Write your code here
     pass
 
 # Exercise 2
 def is_prime(n):
-    # Write your code here
     pass
 
 # Exercise 3
 def count_ending_with_5(n):
-    # Write your code here
     pass
 
 # Exercise 4
 def calculate_balance(input_file,output_file):
-    # Write your code here
     pass
     
 # Exercise 5
