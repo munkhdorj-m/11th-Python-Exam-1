95ea2bf495fa075fe0b8b4bf2da09c5b3d62b8cc
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2025-11-28 00:17:52 +0800

    Update test_assignment.py

--- test_assignment.py
+++ test_assignment.py
@@ -31,17 +31,6 @@ def test2(n, expected):
     assert is_prime(n) == expected
     assert check_contains_loop(is_prime)
 
-
-@pytest.mark.parametrize("lst, expected", [
-    ([5, 15, 23, 25, 40], 3),
-    ([1, 2, 3, 4, 6], 0),
-    ([5, 55, 105], 3),
-    ([10, 20, 30], 0)
-])
-def test3(lst, expected):
-    assert count_ending_with_5(lst) == expected
-    assert check_contains_loop(count_ending_with_5)
-
 @pytest.mark.parametrize(
     "transactions, expected_balance",
     [
@@ -63,7 +52,7 @@ def test3(lst, expected):
         ),
     ],
 )
-def test4(tmp_path, transactions, expected_balance):
+def test3(tmp_path, transactions, expected_balance):
     input_file = tmp_path / "transactions.txt"
     output_file = tmp_path / "summary.txt"
     input_file.write_text(transactions)
@@ -79,7 +68,7 @@ def test4(tmp_path, transactions, expected_balance):
     ("BMW", "M5", [100, 200], 200),       # cannot exceed 200
     ("Honda", "Civic", [0, 0], 0),
 ])
-def test5_1(brand, model, steps, expected_speed):
+def test4_1(brand, model, steps, expected_speed):
     car = Car(brand, model)
     for s in steps:
         car.accelerate(s)
@@ -92,7 +81,7 @@ def test5_1(brand, model, steps, expected_speed):
     ("Audi", "A6", 50, 100, 0),          # cannot go below 0
     ("Ford", "GT", 200, 200, 0),
 ])
-def test5_2(brand, model, accel, brake, expected_speed):
+def test4_2(brand, model, accel, brake, expected_speed):
     car = Car(brand, model)
     car.accelerate(accel)
     car.brake(brake)
@@ -101,11 +90,11 @@ def test5_2(brand, model, accel, brake, expected_speed):
 
 # ---------- Test Status ----------
 @pytest.mark.parametrize("brand, model, accel, brake, expected_status", [
-    ("Toyota", "Supra", 50, 0, "Toyota Supra — Speed: 50 km/h"),
-    ("Nissan", "GTR", 300, 50, "Nissan GTR — Speed: 150 km/h"),
-    ("Ford", "Mustang", 0, 20, "Ford Mustang — Speed: 0 km/h"),
+    ("Toyota", "Supra", 50, 0, "Toyota Supra - Speed: 50 km/h"),
+    ("Nissan", "GTR", 300, 50, "Nissan GTR - Speed: 150 km/h"),
+    ("Ford", "Mustang", 0, 20, "Ford Mustang - Speed: 0 km/h"),
 ])
-def test5_3(brand, model, accel, brake, expected_status):
+def test4_3(brand, model, accel, brake, expected_status):
     car = Car(brand, model)
     car.accelerate(accel)
     car.brake(brake)
