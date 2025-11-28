53f6903ab44450b4d2ce7649d2487dab969a65d7
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2025-11-28 09:30:24 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -7,13 +7,9 @@ def is_prime(n):
     pass
 
 # Exercise 3
-def count_ending_with_5(n):
-    pass
-
-# Exercise 4
 def calculate_balance(input_file,output_file):
     pass
     
-# Exercise 5
+# Exercise 4
 class Car:
     pass
